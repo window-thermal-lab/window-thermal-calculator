4c7662f1256347e9cc4e7e69e3cc9be651d122fb
mm-yamamoto <morimado@morinomado.jp>
2026-03-10 14:36:51 +0900

    add client json loading

--- src/calc.js
+++ src/calc.js
@@ -1,15 +1,21 @@
 // window thermal calculator
 
-const clientId = "mm1001";
+const params = new URLSearchParams(window.location.search);
+const clientId = params.get("client") || "mm1001";
 
+console.log("clientId =", clientId);
+
+if( clientId != NULL )
+{
 fetch(`config/clients/${clientId}.json`)
   .then(r => r.json())
   .then(client => {
-    console.log("client config:", client);
+    console.log("client config =", client);
   });
+}
 
 fetch("config/common.json")
   .then(r => r.json())
   .then(common => {
-    console.log("common config:", common);
+    console.log("common config =", common);
   });
