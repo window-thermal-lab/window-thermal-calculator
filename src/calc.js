87f2cb800479ea90c443c7df75944c4e0eb1d8c6
mm-yamamoto <morimado@morinomado.jp>
2026-04-14 09:56:19 +0900

    update

--- src/calc.js
+++ src/calc.js
@@ -213,7 +213,7 @@ function calculateUw(inputs,selected,config) {
     debuglog("熱抵抗: frameResist 又は sashResistが 0 以下です");
     return null;
   } 
-debuglog("Ug: " + resistSet.frameResist);
+debuglog("Ug: " + areaSet.headArea);
   // コンダクタンス
   const fConductance = (1/resistSet.frameResist)*(areaSet.headArea+areaSet.jambArea+areaSet.sillArea) + (1/resistSet.sashResist)*(areaSet.topRailArea+areaSet.stileArea+areaSet.bottomArea);
   debuglog("木部のコンダクタンス: " + fConductance);
