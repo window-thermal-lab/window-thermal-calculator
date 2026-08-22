77660219d7edecb1bcdb0858458742c5a3829af7
mm-yamamoto <morimado@morinomado.jp>
2026-08-22 17:21:23 +0900

    update

--- src/calc.js
+++ src/calc.js
@@ -733,10 +733,11 @@ function getSlidingAreas(inputs, selected, config) {
   // 左端と右端の2本
   const normalStileCount = 2;
 
-  // 召合せ優先のため、笠木から召合せ幅を除いた長さ
-  const capEffectiveLength =
-    frameInnerWidth
-    - meetingFaceW * meetingCount;
+  // 笠木の幅から縦框の総幅を減算する。
+ const capEffectiveLength =
+  frameInnerWidth
+  - inputs.stileFaceW * normalStileCount
+  - meetingFaceW * meetingCount;
 
   // 召合せ框の高さ
   const meetingHeight =
